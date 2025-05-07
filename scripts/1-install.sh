ede09d6f85e06577ba63063937f2165951f5a114
Walyson Scarazzati da Silva <53382989+walyson-scarazzati@users.noreply.github.com>
2025-05-07 23:14:06 +0100

    Update 1-install.sh

--- scripts/1-install.sh
+++ scripts/1-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PROMETHEUS_VERSION="2.2.1"
+PROMETHEUS_VERSION="3.3.1"
 wget https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION}/prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz
 tar -xzvf prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz
 cd prometheus-${PROMETHEUS_VERSION}.linux-amd64/
