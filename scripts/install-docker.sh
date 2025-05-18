3f6d1c216295c4300bec8613d4bb95bd69be0a36
Walyson Scarazzati da Silva <53382989+walyson-scarazzati@users.noreply.github.com>
2025-05-18 22:09:21 +0100

    Update install-docker.sh

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -3,5 +3,5 @@
 
 curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/2.36.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
